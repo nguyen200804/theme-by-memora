4180b8f3ae55140c7f9d58e27a6a64b3421080a4
nguyen200804 <nguyen200804@gmail.com>
2026-09-24 13:37:03 +0700

    Cập nhật giao diện booking

--- assets/js/booking.js
+++ assets/js/booking.js
@@ -464,7 +464,9 @@
                                 <div class="memora-code-notice">**Quý khách vui lòng lưu lại code chụp để tra cứu</div>
                             </div>
                             <div class="memora-note-card">
-                                <div class="memora-note-tag">Note</div>
+                                <div class="memora-note-tag"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="#91c8f4">
+  <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
+</svg><span>Note</span></div>
                                 <p class="memora-note-p1">
                                     Một lưu ý nhỏ là bạn iu hãy <strong>đến sớm trước 15 phút</strong> so với lịch đã đặt để có thời gian chỉnh lại Makeup và chọn phụ kiện xinh nhaaa
                                 </p>
